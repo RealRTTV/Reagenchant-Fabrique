0ce93ac3b0d11aed38f104366a1b9915bf4693a0
RTTV <tristanandlucky@gmail.com>
2022-01-02 20:54:51 -0500

    <init>

---
+++ src/main/java/ca/rttv/reagenchant/Reagenchant.java
@@ -0,0 +1,14 @@
+package ca.rttv.reagenchant;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Reagenchant implements ModInitializer {
+	public static final Logger Logger = LogManager.getLogger("reagenchant");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
